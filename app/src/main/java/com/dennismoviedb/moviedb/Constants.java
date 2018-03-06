02b4f2a74ef354f854d59584dde933f2456692ac
dennismuturia <dmuturia2922@gmail.com>
2018-03-06 16:00:22 +0300

    I have defined all the variables needed to interact with the MovieDB API. Next will be creating the service.

---
+++ app/src/main/java/com/dennismoviedb/moviedb/Constants.java
@@ -0,0 +1,13 @@
+package com.dennismoviedb.moviedb;
+
+/**
+ * Created by dennis on 3/6/18.
+ */
+
+public class Constants {
+    public static final String movieApi = BuildConfig.MovieAPI;
+    public static final String baseMovieURL = "https://api.themoviedb.org/3/movie/";
+    public static final String popularLocator = "popular";
+    public static final String searchQuery = "search";
+    public static final String baseTvURL = "https://api.themoviedb.org/3/tv/";
+}
