5466ca37c71163e9cc146876b0bf236bf4a97f02
dennismuturia <dmuturia2922@gmail.com>
2018-03-06 15:11:13 +0300

    Initial commit

---
+++ app/src/main/java/com/dennismoviedb/moviedb/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dennismoviedb.moviedb;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    //This is where the main program runs
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
